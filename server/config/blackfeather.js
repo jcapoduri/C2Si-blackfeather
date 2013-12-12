e19f51ae157a57f8809b64a7b13be4aae790a00b
jcapoduri <jcapoduri@gmail.com>
2013-12-12 09:10:08 -0500

    nd library added

---
+++ server/config/blackfeather.js
@@ -0,0 +1,46 @@
+{
+    "meta": {},
+    "config": {
+        "version": "0.1.0", 
+        "system_name": "Black Feather"
+    },
+    "objects": [
+        {
+            "name" : "user",
+            "nd_fields": true,
+            "fields": [
+                {
+                    "name": "username",
+                    "type": "string"
+                },
+                {
+                    "name": "email",
+                    "type": "string"
+                },
+                {
+                    "name": "passwd",
+                    "type": "string"
+                }
+            ]
+        }
+    ],
+    "relations": [],
+    "storage": [
+        {
+            "name": "sql_dev",
+            "type": "database",
+            "db_host": "c2si.com.ar",
+            "db_name": "shaka_dev_blackfeather",
+            "db_user": "shaka_blackfeather",
+            "db_pass": "Pretoria.46",
+            "db_port": 3061,
+            "db_type": "mysql"
+        }
+    ],
+    "apps": {
+        "web": {
+            "storage": ["sql_dev"],
+            "map": {}
+        }
+    }
+}
