faed79f2cb44fe9197a7efa7d404351a5e5227fb
jcapoduri <jcapoduri@gmail.com>
2013-12-18 18:58:33 -0500

    added text plugin to require + module boilerplate

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,17 @@
 // Demo configuration file
 module.exports = function(grunt) {
-  
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      uglify: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        build: {
+          src: 'src/<%= pkg.name %>.js',
+          dest: 'build/<%= pkg.name %>.min.js'
+        }
+      }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 };
