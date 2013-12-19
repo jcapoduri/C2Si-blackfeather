f921f8452a890be3f4d7765681d9226246a30988
jcapoduri <jcapoduri@gmail.com>
2013-12-18 19:02:40 -0500

    basic require config + base index

--- js/config.js
+++ js/config.js
@@ -2,20 +2,26 @@ requirejs.config({
 	baseUrl: 'js',
 	shim: {
 		'backbone': {
-            //These script dependencies should be loaded before loading
-            //backbone.js
             deps: ['underscore', 'jquery'],
-            //Once loaded, use the global 'Backbone' as the
-            //module value.
             exports: 'Backbone'
         },
         'underscore': {
             exports: '_'
+        },
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'bootstrap'
+        },
+        'handlebars': {
+            exports: 'Handlebars'
         }
 	},
 	paths: {
+	    text: 'vendors/requirejs/text',
+	    backbone: 'vendors/backbone/backbone',
 		underscore: 'vendors/underscore/underscore',
 		jquery: 'vendors/jquery/jquery.min',
-		handlebars: 'vendors/handlebars/handlebars.amd'
+		handlebars: 'vendors/handlebars/handlebars',
+		bootstrap: 'vendors/bootstrap/bootstrap.min'
 	}
 });
