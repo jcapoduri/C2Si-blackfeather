b4636ba5fe21e954d5e95d9af807be68dc4ae28a
jcapoduri <jcapoduri@gmail.com>
2014-01-02 15:33:09 -0500

    working grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,35 @@
 // Demo configuration file
 module.exports = function(grunt) {
     grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      uglify: {
-        options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'js/<%= pkg.name %>.js',
+                dest: 'bin/js/<%= pkg.name %>.min.js'
+            }
         },
-        build: {
-          src: 'src/<%= pkg.name %>.js',
-          dest: 'build/<%= pkg.name %>.min.js'
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: "js",
+                    mainConfigFile: "js/config.js",
+                    out: "bin/js/backfeather.js",
+                    name: "app/application",
+                    optimize: 'uglify',
+                    normalizeDirDefines: 'all'
+                }
+            }
         }
-      }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+     // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+    //
+    grunt.registerTask('compile', ['requirejs']);
 };
