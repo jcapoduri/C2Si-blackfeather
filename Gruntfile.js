d7209f269cfbbc4fd505360ecb941e27c872b2b0
jcapoduri <jcapoduri@gmail.com>
2014-01-02 16:56:25 -0500

    grunt update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,13 @@
 // Demo configuration file
 module.exports = function(grunt) {
+    // load tasks
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    //config each task
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
@@ -22,14 +30,29 @@ module.exports = function(grunt) {
                     normalizeDirDefines: 'all'
                 }
             }
-        }
+        },
+        cssmin: {
+          compile: {
+            files: {
+              'bin/styles/style.css': ['styles/normalize.css', 'styles/bootstrap.css', 'styles/bootstrap-theme.css', 'styles/bootstrap-responsive.css', 'styles/yeti.bootstrap.css', 'styles/styles.css']
+            }
+          }
+        },
+        copy: {
+          compile: {
+            files: [
+              {expand: true, src: ['assets/**'], dest: 'bin/'},
+              {expand: true, src: ['fonts/**'], dest: 'bin/'},
+              {expand: true, cwd: 'server/', src: ['**'], dest: 'bin/'}
+            ]
+          }
+        },
+        clean: ['bin/']
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
 
-     // Default task(s).
+     // Registering grunt tasks
     grunt.registerTask('default', ['uglify']);
-    //
-    grunt.registerTask('compile', ['requirejs']);
+
+    grunt.registerTask('compile', ['clean', 'requirejs', 'cssmin', 'copy']);
 };
