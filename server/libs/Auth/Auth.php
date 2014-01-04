c274002cff19982879c4f80ff94870447009bba9
OpenShift guest <anomalia85@gmail.com>
2014-01-04 10:38:25 -0500

    Auth work in progress

--- server/libs/Auth/Auth.php
+++ server/libs/Auth/Auth.php
@@ -1,24 +1,50 @@
 <?php
 
+namespace c2si;
+
 class Auth {
+    /**
+    * esctructura del $handler:
+    *               
+    */
     private $handler;
+    private $token;
 
-    public function __construct(){}
-
+    public function __construct($token = null){
+      $this->token = $token;
+    }
+    
+    public function setHandler($handler){ $this->handler = $handler; }
+    
     /**
     * @param: token: access token a validar
     * @return: boolean: so el token es valido o no, de ser valido, renovar la fecha de expiración
     */
-    public function authentificate($token = $HTTP['auth-token']){
-
+    public function authentificate($token = null){
+      $_token = $this->$token;
+      if (!is_null($token)) $_token = $token;
+      
+      if ($this->handler->query("SELECT id_usr FROM auth_tokens WHERE expiration > CURRENT_TIMESTAMP AND token LIKE '".$token . "")){
+          $this->handler->query("UPDATE user SET expiration = CURRENT_TIMESTAMP");
+          return true;
+      }else{
+          return false;
+      };
     }
 
     /**
     * @param: token: access token a validar
     * @return: id del usuario del token o -1
     */
-    public function getUserId($token = $HTTP['auth-token']) {
-
+    public function getUserId($token = null) {
+      $_token = $this->$token;
+      if (!is_null($token)) $_token = $token;
+      if ($this->handler->query("SELECT id_usr FROM auth_tokens WHERE expiration > CURRENT_TIMESTAMP AND token LIKE '". $token . "'")){
+          $row = $result->fetch_array($result);
+          return $row['id_usr'];
+      }else{
+          return 0;
+      }
     }
 
     /**
@@ -26,7 +52,15 @@ class Auth {
     * @return: string, token unico de acceso con 1hora de expiración
     */
     public function generateToken($userid) {
-
+      $token = "";
+      if (!$userid) return $token;
+      $insertedToekn = false;
+      do {
+        $token = bin2hex(openssl_random_pseudo_bytes(16));
+        $insertedToken = $this->handler->query("INSERT INTO auth_token (token, expiration, id_usr) VALUES ('".$newToken."', NOW() + INTERVAL 1 HOUR, " . $userid . ")");
+      } while (!$insertedToken);
+      
+      return $token;
     }
 };
 
