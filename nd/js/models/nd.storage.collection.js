467e926333101bfdcdab7c021073f4cee646a823
jcapoduri <jcapoduri@gmail.com>
2014-03-11 20:10:08 -0300

    remove nd

--- nd/js/models/nd.storage.collection.js
+++
@@ -1,7 +0,0 @@
-define(['jquery', 'underscore', 'backbone', 'models/nd.storage'], function($, _, Backbone, storageModel){
-    var collection = Backbone.Collection.extend({
-        model: storageModel
-    });
-
-    return collection;
-});
--- nd/js/models/nd.storage.js
+++
@@ -1,9 +0,0 @@
-define(['jquery', 'underscore', 'backbone'], function($, _, Backbone){
-    var config = Backbone.Model.extend({
-        defaults: {
-            "system_name" : "",
-            "version": ""
-        }
-    });
-    return config;
-});
