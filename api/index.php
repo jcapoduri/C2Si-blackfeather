681ce6362a59816d0f1a5c399f23abdda0138cd3
Jorge Capoduri <jcapoduri@gmail.com>
2014-05-13 23:21:49 -0300

    fix

--- api/index.php
+++ api/index.php
@@ -7,57 +7,32 @@ ini_set('display_errors', 1);
 
 date_default_timezone_set('UTC');
 
-// SLIM Framework, to provide REST capability and scalfolding
 require_once 'libs/Slim/Slim.php';
-// small propietary ORM
 require_once 'libs/RedBean/rb.phar';
-// small propietary Auth
 require_once 'libs/Auth/Auth.php';
 require_once 'libs/Auth/AuthMiddleware.php';
 
-
-class response {
-	public $status = false;
-    public $message = "";
-    public $response = "";
-    public $error_no = 0;
-
-    public function toJson(){
-            return json_encode($this);
-    }
-
-    protected static function generateResponse($message, $response = "", $errno = 0) {
-        $res = new response();
-        $res->status = true;
-        $res->message = $message;
-        $res->response = $response;
-        $res->error_no = $errno;
-        return $res;
-    }
-
-    public static function pass($message, $response = "", $errno = 0) {
-        $res = response::generateResponse($message, $response, $errno);
-        $res->status = true;
-        return $res;
-    }
-
-    public static function fail($message, $response = "", $errno = 0) {
-        $res = response::generateResponse($message, $response, $errno);
-        $res->status = false;
-        return $res;
-    }
-}
+// models
+foreach (glob("models/*.php") as $filename)
+{
+    require_once $filename;
+};
 
 // SLIM setting up
 \Slim\Slim::registerAutoloader();
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+    'cookies.encrypt' => true,
+    'cookies.secret_key' => 'my_secret_key',
+    'cookies.cipher' => MCRYPT_RIJNDAEL_256,
+    'cookies.cipher_mode' => MCRYPT_MODE_CBC
+));
 
 error_reporting(E_ALL);
 
 //set Auth middleware
 //$app->add(new AuthMiddleware());
 
-R::setup('mysql:host=localhost;dbname=blackfeather', 'root', '');
+R::setup('mysql:host=localhost;dbname=blackfeather', 'root', 'Kotipelto.46');
 //R::setup('mysql:host=127.11.188.129;dbname=c9', 'jcapoduri', '');
 //R::nuke();
 // Noop - no operation (for testing)
@@ -67,9 +42,10 @@ $app->get('/noop', function () use ($app){
     $response->write('{}');
 });
 
-require_once 'security/access.php';
-require_once 'security/register.php';
-
+foreach (glob("routes/*.php") as $filename)
+{
+    require_once $filename;
+};
 //R::freeze(TRUE);
 
 
